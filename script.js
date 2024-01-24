80c7c98bd87c835134eb987f02f0f6e1194c9371
Yusuf <holla2specialyusuf@gmailcom>
2024-01-24 03:05:03 +0100

    add history and about section

---
+++ script.js
@@ -0,0 +1,19 @@
+// Tabbed Menu
+
+function openMenu(event, menuName) {
+    let menuArray = document.getElementsByClassName("menu");
+    for (let i = 0; i < menuArray.length; i++) {
+        menuArray[i].style.display = "none";
+    }
+
+    let tablinks = document.getElementsByClassName("tablink");
+    for (let i = 0; i < tablinks.length; i++) {
+        tablinks[i].classList.remove("active-tab")
+    }
+
+    document.getElementById(menuName).style.display = "block";
+    event.currentTarget.classList.add("active-tab");
+}
+
+document.getElementById("mainLink").click();
+
